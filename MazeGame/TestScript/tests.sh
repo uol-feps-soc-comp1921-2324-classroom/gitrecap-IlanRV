c8375bc0967cf3baeca21987d5e8c8264b2a078f
IlanRV <146100248+IlanRV@users.noreply.github.com>
2024-02-29 02:58:52 +0000

    Test scripts and maze maps done

---
+++ MazeGame/TestScript/tests.sh
@@ -0,0 +1,150 @@
+#!/bin/bash
+
+echo "--Testing control functions--"
+
+echo -n "Testing for invalid control input - "
+./MainGame < notacontrol > tmp
+
+if grep -q "Expected output or error message" tmp;
+then
+    echo "PASS"
+else
+    echo "FAIL"
+fi
+
+
+echo -n "Testing for double start  - "
+./MainGame < doublestart.txt > tmp
+
+if grep -q "Expected output or error message" tmp;
+then
+    echo "PASS"
+else
+    echo "FAIL"
+fi
+
+
+echo -n "Testing for invalid mapping  - "
+./MainGame < invalidmap > tmp
+
+if grep -q "Expected output or error message" tmp;
+then
+    echo "PASS"
+else
+    echo "FAIL"
+fi
+
+
+echo -n "Testing for unloaded map  - "
+./MainGame < mapnotloaded > tmp
+
+if grep -q "Expected output or error message" tmp;
+then
+    echo "PASS"
+else
+    echo "FAIL"
+fi
+
+
+echo -n "Testing for invalid mapping  - "
+./MainGame < mapnotloaded > tmp
+
+if grep -q "Expected output or error message" tmp;
+then
+    echo "PASS"
+else
+    echo "FAIL"
+fi
+
+
+echo -n "Testing for double finish  - "
+./MainGame < doublefinish > tmp
+
+if grep -q "Expected output or error message" tmp;
+then
+    echo "PASS"
+else
+    echo "FAIL"
+fi
+
+echo -n "Testing for player inbounds  - "
+./MainGame < playerinbounds.txt > tmp
+
+if grep -q "Expected output or error message" tmp;
+then
+    echo "PASS"
+else
+    echo "FAIL"
+fi
+
+
+echo -n "Testing for double map  - "
+./MainGame < doublemap.txt > tmp
+
+if grep -q "Expected output or error message" tmp;
+then
+    echo "PASS"
+else
+    echo "FAIL"
+fi
+
+
+echo -n "Testing for double map  - "
+./MainGame < doublemap.txt > tmp
+
+if grep -q "Expected output or error message" tmp;
+then
+    echo "PASS"
+else
+    echo "FAIL"
+fi
+
+echo -n "Testing for no exit  - "
+./MainGame < noexit.txt > tmp
+
+if grep -q "Expected output or error message" tmp;
+then
+    echo "PASS"
+else
+    echo "FAIL"
+fi
+
+
+echo -n "Testing for correct input  - "
+./MainGame < correctinput > tmp
+
+if grep -q "Expected output or error message" tmp;
+then
+    echo "PASS"
+else
+    echo "FAIL"
+fi
+
+
+echo -n "Testing for wall hit  - "
+./MainGame < hitwall > tmp
+
+if grep -q "Expected output or error message" tmp;
+then
+    echo "PASS"
+else
+    echo "FAIL"
+fi
+
+
+echo -n "Testing for player movement accuracy   - "
+./MainGame < accurateinput > tmp
+
+if grep -q "Expected output or error message" tmp;
+then
+    echo "PASS"
+else
+    echo "FAIL"
+fi
+
+
+
+rm -f tmp
+
+
+
